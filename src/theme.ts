bfdd005c79928e7b0c3d81963e73817e2bfae2ba
Mikolaj Gawlik <mikolaj3313@gmail.com>
2023-09-28 14:58:49 +0200

    Initial commit

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({
+  config,
+  styles: {
+    global: {
+      body: {
+        bg: '#F5F5F5',
+      }
+    }
+  }
+});
+
+export default theme;
