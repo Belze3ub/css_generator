952f4c764d06154921e17ce911752a9ec7aacab0
Mikolaj Gawlik <mikolaj3313@gmail.com>
2023-11-09 12:12:16 +0100

    Added vercel file for redirects

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
-import { Box, Flex, Switch, useColorMode, Text } from "@chakra-ui/react"
+import { Box, Flex, Switch, useColorMode, Text } from '@chakra-ui/react';
 
 const Navbar = () => {
+  // const { colorMode, toggleColorMode } = useColorMode();
   const { toggleColorMode } = useColorMode();
   return (
     <Box bg={'#1f2024'} px={'1rem'} py={'2rem'} color={'white'}>
@@ -8,11 +9,12 @@ const Navbar = () => {
         <Text>CSS Generator</Text>
         <Box display={'flex'} alignItems={'center'}>
           <Text>Color mode</Text>
+          {/* {colorMode === 'dark' ? 'Light mode' : 'Dark mode'} */}
           <Switch onChange={toggleColorMode} paddingLeft={'.5rem'} />
         </Box>
       </Flex>
     </Box>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
